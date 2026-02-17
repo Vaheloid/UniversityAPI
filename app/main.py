7b4bfce62b954d7ace6bace3efbdf2d930cf6345
Vaheloid <vlad.tyupin@yandex.ru>
2026-02-17 22:55:03 +0500

    added pydantic schemas + fix

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+from typing import List
+
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from app.models import SStudent
 from utils import json_to_dict_list
 import os
 
@@ -16,6 +20,11 @@ path_to_json = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__fi
 
 app = FastAPI()
 
+class RBStudent:
+    def __init__(self, course: int, major: str | None = None, enrollment_year: int | None = 2018):
+        self.course = course
+        self.major: str | None = major
+        self.enrollment_year: int | None = enrollment_year
 @app.get("/students")
 def get_all_students(course: int | None = None):
     students = json_to_dict_list(path_to_json)
@@ -28,15 +37,23 @@ def get_all_students(course: int | None = None):
                 return_list.append(student)
         return return_list
 @app.get("/students/{course}")
-def get_all_students_course(course: int, major: str | None = None, enrollment_year: int | None = 2018):
+def get_all_students_course(request_body: RBStudent = Depends()) -> List[SStudent]:
     students = json_to_dict_list(path_to_json)
     filtered_students = []
     for student in students:
-        if student['course'] == course:
+        if student['course'] == request_body.course:
             filtered_students.append(student)
 
-    if major:
-        filtered_students = [student for student in filtered_students if student['major'].lower() == major.lower()]
-    if enrollment_year:
-        filtered_students = [student for student in filtered_students if student['enrollment_year'] == enrollment_year]
+    if request_body.major:
+        filtered_students = [student for student in filtered_students if student['major'].lower() == request_body.major.lower()]
+    if request_body.enrollment_year:
+        filtered_students = [student for student in filtered_students if student['enrollment_year'] == request_body.enrollment_year]
     return filtered_students
+
+@app.get("/student", response_model=SStudent)
+def get_student_from_param_id(student_id: int):
+    students = json_to_dict_list(path_to_json)
+    for student in students:
+        if student['student_id'] == student_id:
+            return student
+    return None
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+from datetime import date
+
 import requests
+from pydantic import ValidationError
+
+from app.models import Student
 
 
 # def get_all_students():
@@ -35,11 +40,34 @@ import requests
 #     print(student)
 
 #параметр пути + параметр запроса
-def get_students_with_param_mix(course: int, major: str, enrollment_year: int):
-    url = f"http://127.0.0.1:8000/students/{course}"
-    response = requests.get(url, params={"major": major, "enrollment_year": enrollment_year})
-    return response.json()
+# def get_students_with_param_mix(course: int, major: str, enrollment_year: int):
+#     url = f"http://127.0.0.1:8000/students/{course}"
+#     response = requests.get(url, params={"major": major, "enrollment_year": enrollment_year})
+#     return response.json()
+#
+#
+# students = get_students_with_param_mix(2, major=None, enrollment_year=2018)
+# print(students)
+
+def test_valid_student(data: dict) -> None:
+    try:
+        student = Student(**data)
+        print(student)
+    except ValidationError as e:
+        print(f"Ошибка валидации: {e}")
 
 
-students = get_students_with_param_mix(2, major=None, enrollment_year=2018)
-print(students)
+# student_data = {
+#     "student_id": 1,
+#     "phone_number": "+1234567890",
+#     "first_name": "Иван",
+#     "last_name": "Иванов",
+#     "date_of_birth": date(2000, 1, 1),
+#     "email": "ivan.ivanov@example.com",
+#     "address": "Москва, ул. Пушкина, д. Колотушкина",
+#     "enrollment_year": 2022,
+#     "major": "Информатика",
+#     "course": 3,
+# }
+#
+# print(test_valid_student(student_data))
